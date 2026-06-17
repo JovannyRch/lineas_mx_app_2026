3540640a078223379a6d66569d8acf157cb999db
jovannyrch <jovannyrch@gmail.com>
2026-06-17 12:06:17 -0600

    feat: update AdMob app IDs for production configuration

--- src/constants/ads.ts
+++ src/constants/ads.ts
@@ -2,7 +2,9 @@ import { Platform } from "react-native";
 import { TestIds } from "react-native-google-mobile-ads";
 
 const productionBannerAdUnitId = Platform.select({
-  android: process.env.EXPO_PUBLIC_ADMOB_ANDROID_BANNER_UNIT_ID,
+  android:
+    process.env.EXPO_PUBLIC_ADMOB_ANDROID_BANNER_UNIT_ID ??
+    "ca-app-pub-4665787383933447/1522504220",
   ios: process.env.EXPO_PUBLIC_ADMOB_IOS_BANNER_UNIT_ID,
 });
 
